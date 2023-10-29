9ef8757af8cdd5b97943da4551aae4e85f038816
victoria <v.suprun1961@gmail.com>
2023-10-29 22:04:08 +0200

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str) {
+  const bracketsConfig = ['(', '{', '[', '|'];
+  const PAIRED_BRACKETS = {
+    [')']:'(',
+    ['}']:'{',
+    [']']:'[',
+    ['|']:'|',
+  };
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    let currentSymbol = str[i];
+
+    if (bracketsConfig.includes(currentSymbol)) {
+      stack.push(currentSymbol);
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+
+      let topElement = stack[stack.length - 1];
+      if (PAIRED_BRACKETS[currentSymbol] === topElement) {
+        stack/pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
 }
